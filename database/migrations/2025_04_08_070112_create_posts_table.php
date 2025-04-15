96113f64b209c7d61c47f04f863f5342ac39f6e8
Tauhid219 <rezatawhid@gmail.com>
2025-04-15 13:13:49 +0600

    Add migrations for categories, posts, tags, and post_tag with necessary fields and relationships

--- database/migrations/2025_04_08_070112_create_posts_table.php
+++ database/migrations/2025_04_08_070112_create_posts_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,13 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->text('excerpt')->nullable();
+            $table->longText('body');
+            $table->string('slug')->unique();
+            $table->foreignId('category_id')->nullable()->constrained()->onDelete('set null');
+            $table->boolean('is_published')->default(false);
             $table->timestamps();
         });
     }
--- database/migrations/2025_04_08_070829_create_post_tag_table.php
+++ database/migrations/2025_04_08_070829_create_post_tag_table.php
@@ -4,16 +4,16 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('post_tag', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->foreignId('post_id')->constrained()->onDelete('cascade');
+            $table->foreignId('tag_id')->constrained()->onDelete('cascade');
+            $table->primary(['post_id', 'tag_id']);
         });
     }
 
